4c9f4bd54e3fd0f6b5a0e76fb587bde073d6b2cb
Jung jinseong <wjdwl545@naver.com>
2024-10-23 22:54:53 +0900

    s

--- app/(commom)/CRUD/CopyUrl.tsx
+++ app/(commom)/CRUD/CopyUrl.tsx
@@ -2,7 +2,7 @@
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import { usePathname } from 'next/navigation'
 
-export default async function CopyUrl(){
+export default function CopyUrl(){
     const pathname = usePathname()//have to :: 앞에 도메인 넣기. 현재 /fdsfsd?=fsdfsd형태로만 복사됌
     const copyToClip = ()=> {
         navigator.clipboard.writeText(pathname)
