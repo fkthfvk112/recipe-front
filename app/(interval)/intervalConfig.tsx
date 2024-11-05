e17bae6cc09808a53976d5a3fb2a531a4163e18a
Jung jinseong <wjdwl545@naver.com>
2024-11-05 20:35:29 +0900

    finding token error

--- app/(interval)/intervalConfig.tsx
+++ app/(interval)/intervalConfig.tsx
@@ -15,46 +15,46 @@ interface Token{
 }
 
 function IntervalConfig(){
-    const [isSignIn, setIsSignIn] = useRecoilState(siginInState);
-    function loginChk(){
-        const refreshTokenStr = getCookie("refresh-token");
+    // const [isSignIn, setIsSignIn] = useRecoilState(siginInState);
+    // function loginChk(){
+    //     const refreshTokenStr = getCookie("refresh-token");
 
-        const setLogOut = ()=>{
-            setIsSignIn(false);
-            deleteCookie("refresh-token");
-            deleteCookie("authorization");
-        }
+    //     const setLogOut = ()=>{
+    //         setIsSignIn(false);
+    //         deleteCookie("refresh-token");
+    //         deleteCookie("authorization");
+    //     }
 
-        if(refreshTokenStr){
-            const refreshToken:Token = jwtDecode(refreshTokenStr);
-            const expDate = new Date(refreshToken.exp * 1000);
+    //     if(refreshTokenStr){
+    //         const refreshToken:Token = jwtDecode(refreshTokenStr);
+    //         const expDate = new Date(refreshToken.exp * 1000);
 
-            if(isNaN(expDate.getTime())){
-                setLogOut();
-                return;
-            }
+    //         if(isNaN(expDate.getTime())){
+    //             setLogOut();
+    //             return;
+    //         }
 
-            if (expDate.getTime() < Date.now()) { //리프래시 토큰 만료
-                setLogOut();
-                Swal.fire({
-                    title: "로그인 유효시간",
-                    text:"로그인 유효시간이 만료되었습니다.",
-                    icon: "info",
-                    confirmButtonText: "확인",
-                    allowEnterKey:false
-                  })
-                return;
-            }
+    //         if (expDate.getTime() < Date.now()) { //리프래시 토큰 만료
+    //             setLogOut();
+    //             Swal.fire({
+    //                 title: "로그인 유효시간",
+    //                 text:"로그인 유효시간이 만료되었습니다.",
+    //                 icon: "info",
+    //                 confirmButtonText: "확인",
+    //                 allowEnterKey:false
+    //               })
+    //             return;
+    //         }
             
-            setIsSignIn(true); //모두 해당 없으면 로그인 상태 유지
-        }else{
-            if(isSignIn){
-                setIsSignIn(false);
-            }
-        }
-    }
+    //         setIsSignIn(true); //모두 해당 없으면 로그인 상태 유지
+    //     }else{
+    //         if(isSignIn){
+    //             setIsSignIn(false);
+    //         }
+    //     }
+    // }
 
-    useInterval(loginChk, 1000, "intervalKey#loginChk")
+    // useInterval(loginChk, 1000, "intervalKey#loginChk")
     
     return <></>
 }
