6b9aadef77fd6006d73e5ae9e32ceceeffb43d2c
Jung jinseong <wjdwl545@naver.com>
2026-03-23 01:32:29 +0900

    fix

--- app/(user)/signin/naver/NaverLogin.tsx
+++ app/(user)/signin/naver/NaverLogin.tsx
@@ -18,8 +18,8 @@ const NaverLogin = () => {
       localStorage.setItem('naverCsrf', csrfUUID);
       const redirect_uri = process.env.NODE_ENV === 'development' ? 'https://localhost:3001/signin/naver/callback' : 'https://mug-in.com/signin/naver/callback';
 
-      location.href = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=683zYUF6rA106NQeQTNm&state=${csrfUUID}&redirect_uri=${redirect_uri}&auth_type=reauthenticate`
-    }
+      location.href = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=683zYUF6rA106NQeQTNm&state=${csrfUUID}&redirect_uri=${redirect_uri}`
+    }//다시 로그인 시키려면 auth_type=reauthenticate
   
     return <div className="mt-3 cursor-pointer w-[50px] h-[50px]" onClick={naverLogin}>
       <Image 
